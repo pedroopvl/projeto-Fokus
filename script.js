813ec4f92a456d00df4d350885210e5bb2b51666
Pedro Paulo <pedropottergd@gmail.com>
2025-03-21 15:00:33 -0300

    configurando o temporizador e adicionando efeitos sonoros

--- script.js
+++ script.js
@@ -5,9 +5,16 @@ const longoBt = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
 const titulo = document.querySelector('.app__title')
 const botoes = document.querySelectorAll('.app__card-button')
+const startPauseBt = document.querySelector('#start-pause')
 const musicaFocoInput = document.querySelector('#alternar-musica')
+const audioPlay = new Audio('/sons/play.wav');
+const audioPausa = new Audio('/sons/pause.mp3');
+const audioTempoFinalizado = new Audio('./sons/beep.mp3')
 const musica = new Audio('/sons/luna-rise-part-one.mp3')
 musica.loop = true
+
+let tempoDecorridoEmSegundos = 5
+let intervaloId = null
 
 musicaFocoInput.addEventListener('change', () => {
     if(musica.paused) {
@@ -59,3 +66,29 @@ longoBt.addEventListener('click', () => {
     alterarContexto('descanso-longo')
     longoBt.classList.add('active')
 })
+
+const contagemRegressiva = () => {
+    if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        zerar()
+        return
+    }
+    tempoDecorridoEmSegundos -= 1
+}
+
+startPauseBt.addEventListener('click', iniciarOuPausar)
+
+function iniciarOuPausar () {
+    if (intervaloId) {
+        audioPausa.play();
+        zerar()
+        return
+    }
+    audioPlay.play();
+    intervaloId = setInterval(contagemRegressiva, 1000)
+}
+
+function zerar () {
+    clearInterval(intervaloId)
+    intervaloId = null
+}
